1b1fba93874ffa5dcf08fff122da647bad05d1cf
Sumityadav9383 <yadavsumit22407@gmail.com>
2024-10-21 00:06:34 +0530

    Update input.c

--- input.c
+++ input.c
@@ -3,8 +3,7 @@
 int main(){
     int a; 
     printf("enter the value of a:");
-    scanf("%d", &a);//this function is is used for taking input from user 
-    // %d = int %f= float   %c= char
+    scanf("%d", &a);
     printf("The value of a is %d", a);
     return 0;
 }
