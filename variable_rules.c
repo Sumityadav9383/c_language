c062f38bf0e1a14dd70e93c37ed748bb0701adf2
Sumityadav9383 <yadavsumit22407@gmail.com>
2024-10-21 00:09:51 +0530

    odd/even.c

--- variable_rules.c
+++ variable_rules.c
@@ -1,11 +1,17 @@
-#include<stdio.h>
-
-int main(){
-    int shitesh; //this is allowed 
-    int _shitesh; //this is allowed 
-    int shitesh_07; //this is allowed 
-    // int 0shitesh;   this is not allowed 
-    //int #shitesh;   this is not allowed 
-
-    return 0;
+//even or odd finder//
+ #include<stdio.h>
+int main()
+{
+int a;
+printf("enter the value of a");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("The number is even number");
 }
+else
+{
+printf("The number is odd number");
+}
+return 0;
+} 
